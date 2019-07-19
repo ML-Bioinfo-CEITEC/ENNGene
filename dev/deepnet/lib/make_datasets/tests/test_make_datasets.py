796de24a9c559f7fe4c78d9fa909b3f251585bf4
Daniel Kuchta <kuchtadan@gmail.com>
2019-07-19 11:32:42 +0200

    "Removing valid_data from make_datasets and re-writing test.

--- dev/deepnet/lib/make_datasets/tests/test_make_datasets.py
+++ dev/deepnet/lib/make_datasets/tests/test_make_datasets.py
@@ -12,8 +12,8 @@ class TestMakeDatasets(object):
     def create_data(self):
         test_data = []
         for default_args in create_random_arguments(self.arguments_shuffles):
-            _, valid_data = make_datasets(default_args)
-            test_data.append(valid_data)
+            datasets = make_datasets(default_args)
+            test_data.append(datasets)
         yield test_data
 
 
@@ -21,27 +21,32 @@ class TestMakeDatasets(object):
         """Test if corresponding datasets in different branches have same length"""
         for test_data in create_data:
             if len(test_data) > 1:  # more than 1 branch
+            branch_1, branch_2 = list(test_data.keys())
                 for dataset in self.datasets_names:
-                    assert len(test_data[0][dataset]) == len(test_data[1][dataset])
+                    if dataset == 'train':
+                        continue
+                    assert len(test_data[branch_1][dataset].dictionary) == \
+                           len(test_data[branch_2][dataset].dictionary)
 
 
     def test_output_datasets_is_empty(self, create_data):
         """Test if any of output datasets is empty"""
         for test_data in create_data:
-            for branch in test_data:
+            for branch in test_data.keys():
                 for dataset in self.datasets_names:
-                    assert branch[dataset]
-
-
-    @pytest.mark.parametrize('expected', [set])
-    def test_datasets_data_type(self, create_data, expected):
-        for test_data in create_data:
-            for branch in test_data:
-                for dataset in self.datasets_names:
-                    assert type(branch[dataset]) is expected
+                    assert test_data[branch][dataset].dictionary
 
 
     @pytest.mark.parametrize('expected', [list])
+    def test_datasets_data_type(self, create_data, expected):
+        for test_data in create_data:
+            for branch in test_data.keys():
+                for dataset in self.datasets_names:
+                    assert all(type(value) is expected for value \
+                        in test_data[branch][dataset].dictionary.values())
+
+
+    @pytest.mark.parametrize('expected', [dict])
     def test_output_data_type(self, create_data, expected):
         for test_data in create_data:
             assert type(test_data) is expected
