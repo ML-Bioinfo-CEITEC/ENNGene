8706840b3308eb44b9f26531200f9c0aa68b832d
Eliska <eliskamalanikova@gmail.com>
2019-10-03 17:53:05 +0200

    Parse gpu and cpu options

--- lib/utils/subcommand.py
+++ lib/utils/subcommand.py
@@ -9,13 +9,22 @@ class Subcommand:
     def __init__(self, parser):
         self.args = parser.parse_args(sys.argv[2:])
 
+        self.gpu = self.args.gpu
+
+        if type(self.args.ncpu) == int:
+            self.ncpu = self.args.ncpu
+        elif self.args.ncpu == 'auto':
+            # TODO detect number of available cpus
+            self.ncpu = 4
+        else:
+            raise Exception("Invalid value for number of CPUs.")
+
         if self.args.output:
             self.output_folder = self.args.output
         else:
             self.output_folder = os.path.join(os.getcwd(), 'output')
         self.ensure_dir(self.output_folder)
 
-        self.ncpu = self.args.ncpu
         self.verbose = self.args.verbose
 
     @staticmethod
@@ -30,10 +39,15 @@ class Subcommand:
             help="Specify folder for output files. If not specified, current working directory will be used."
         )
         parser.add_argument(
-            # TODO allow option 'auto' to use all available
             "--ncpu",
             default=1,
-            help="Number of CPUs to be used for parallelized processes. Default = 1."
+            help="Number of CPUs to be used. An integer or 'auto' to use all available cpus. Default: 1."
+        )
+        parser.add_argument(
+            "--gpu",
+            help="Use available GPU. Default: False.",
+            default=False,
+            type=bool
         )
 
         # TODO do we need verbose and quiet, or rather use the logger for everything?
